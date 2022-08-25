f15e4b338ffe8ed70e766ca4edf9811f2da5862d
unknown <djiju5@gmail.com>
2022-08-25 16:11:32 +0530

    initial commit

---
+++ crms/crms/base.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   
+        'PORT': '3306',
+    }    
+}
---
+++ crms/crms/models.py
@@ -0,0 +1,6 @@
+import pymysql
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+pymysql.install_as_MySQLdb()
